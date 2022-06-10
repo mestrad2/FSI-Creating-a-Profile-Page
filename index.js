89d87326711819e826fcc0e09be2421b3295e77b
Manuel Estrada <manuel.estrada16@gmail.com>
2022-06-09 20:16:27 -0400

    created index.js page

--- index.js
+++ index.js
@@ -13,10 +13,39 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
+
+let dogDetailsHeader = document.createElement('h3')
+dogDetailsHeader.textContent = 'Description:'
+dogDetails.append(dogDetailsHeader)
+
+let dogDescription = document.createElement('p')
+dogDescription.textContent = `This gentle dog is aloof toward her owner, and never comes when called.
+She always acts as though any stranger she meets will harm her, and
+dislikes other animals.`
+
+dogDetails.append(dogDescription)
+
+let dogFeedTimes = document.createElement('h3')
+dogFeedTimes.textContent = 'Feeding Times:'
+dogDetails.append(dogFeedTimes)
+
+let dogTimesList = document.createElement('ul')
+dogDetails.append(dogTimesList)
 
 
+let dogFeedTime1 = document.createElement('li')
+let dogFeedTime2 = document.createElement('li')
+let dogFeedTime3 = document.createElement('li')
+
+dogFeedTime1.textContent = "9:00 am"
+dogFeedTime2.textContent = "12:00 pm"
+dogFeedTime3.textContent = "5:00 pm"
+
+dogTimesList.append(dogFeedTime1)
+dogTimesList.append(dogFeedTime2)
+dogTimesList.append(dogFeedTime3)
